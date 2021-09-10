7a8cbc4840acfc8e1877b646fac965976b0dcb42
Макс <yashanty@gmail.com>
2021-09-10 16:52:01 +0300

    Add project files.

---
+++ Cars.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLinq1
+{
+    record Car (string marka, string model, int year, string color)
+    {
+        public override string ToString()
+        {
+            return $"Марка {marka}, модель {model}, год выпуска {year}, цвет {color}.";
+        }
+    }
+
+    record Customer(string name, string model, string tel)
+    {
+        public override string ToString()
+        {
+            return $"Покупатель {name}, модель {model}, телефон {tel}.";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarLinq1
+{
+    class Program
+    {
+        
+        
+        
+        static void Main(string[] args)
+        {
+            var listCars = new List<Car>
+            {
+                new ("Fiat", "Bravo", 2005, "red"),
+                new ("Mersedes", "E", 2010, "black"),
+                new ("Skoda", "Fabia", 2009, "yellow"),
+                new ("Mersedes","A", 2009, "grey")
+
+            };
+
+            var listCustomers = new List<Customer>
+            {
+                new ("Petrov", "Mersedes", "0509864578"),
+                new ("Ivanov", "Fiat", "0509876545"),
+                new ("Vasiliev", "Skoda", "0504789863")
+
+            };
+
+            var markaAndColor = 
+                from cars in listCars
+                select new {Marka = cars.marka, Color = cars.color};
+
+            foreach (var item in markaAndColor)
+            {
+                Console.WriteLine(item.Marka+" "+item.Color);
+            }
+
+            Console.WriteLine();
+
+            var custumerCar = 
+                from custumers in listCustomers
+                join cars in listCars on custumers.model equals cars.marka
+                select new
+                {
+                    Name = custumers.name,
+                    Tel = custumers.tel,
+                    Model = custumers.model,
+                    Brand = cars.marka,
+                    Color = cars.color,
+                    Year = cars.year
+                };
+            foreach (var item in custumerCar)
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4} {5}", item.Name, item.Tel, item.Model, item.Brand, item.Color, item.Year);
+            }
+        }
+    }
+}
